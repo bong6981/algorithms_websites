ecb0fb32691da4ca44e9b752f62c14161ab6167e
bong6981 <kbk5190@naver.com>
2021-07-14 17:26:03 +0900

    done sum_plus_minus.py

---
+++ bongf/sum_plus_minus.py
@@ -0,0 +1,12 @@
+def solution(absolutes, signs):
+    answer = 0
+    for x in range(len(absolutes)) :
+        if(signs[x] == True) :
+            answer += absolutes[x]
+        else :
+            answer -= absolutes[x]         
+    return answer
+
+absolutes = [1,2,3]	
+signs = [False,False,True]
+print(solution(absolutes, signs))
