71c5ab3c66dabb79a20c44e0a6e8611aa5f0cf87
bong6981 <kbk5190@naver.com>
2021-07-14 16:18:10 +0900

    done innerProduct

--- java/src/InnerProduct.java
+++ java/src/InnerProduct.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class InnerProduct {
+public class InnerProduct {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4};
+        int[] b = {-3,-1,0,2};
+        System.out.println(solution(a,b));
+    }
+
+    public static int solution(int[] a, int[] b) {
+        int answer = 0;
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+        return answer;
+    }
 }
