ae0599ef237c804db0a87baeb9c050e3de3fe811
bong6981 <kbk5190@naver.com>
2021-07-14 17:06:11 +0900

    done clawCrane

---
+++ java/src/clawCrane.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class clawCrane {
+    public static void main(String[] args) {
+        int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+        int[] moves = {1,5,3,5,1,2,1,4};
+        System.out.println(solution(board, moves));
+    }
+
+    public static int solution(int[][] board, int[] moves) {
+        List<Integer> resultList = new ArrayList<Integer>();
+        int count = 0;
+        for( int column : moves) {
+            for(int j=0; j<board.length; j++) {
+                if(board[j][column] != 0) {
+                    if(resultList.size() != 0 && resultList.get(resultList.size()-1) == board[j][column]) {
+                        count += 2;
+                        resultList.remove(resultList.size() -1);
+                        board[j][column] = 0;
+                        break;
+                    } else {
+                        resultList.add(board[j][column]);
+                        board[j][column] = 0;
+                        break;
+                    }
+                }
+            }
+        }
+
+        int answer = count;
+        return answer;
+    }
+}
