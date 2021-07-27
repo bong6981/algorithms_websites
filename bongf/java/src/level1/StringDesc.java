9f4d55ba20f3ffc6fd9fb41755887b58f1e85af5
bong6981 <kbk5190@naver.com>
2021-07-27 18:28:34 +0900

    Delete unused import in StringDesc

--- bongf/java/src/level1/StringDesc.java
+++ bongf/java/src/level1/StringDesc.java
@@ -1,8 +1,6 @@
 package level1;
 
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
