cbe7d075e238e4f493c50ee6b2a2bb65d689dbd9
bong6981 <kbk5190@naver.com>
2021-08-01 22:52:16 +0900

    StringToInt.java(문자열을정수로바꾸기)

---
+++ bongf/java/src/level1/StringToInt.java
@@ -0,0 +1,11 @@
+package level1;
+
+public class StringToInt {
+    public static void main(String[] args) {
+        System.out.println(solution("-1234"));
+    }
+
+    public static int solution(String s) {
+        return Integer.parseInt(s);
+    }
+}
