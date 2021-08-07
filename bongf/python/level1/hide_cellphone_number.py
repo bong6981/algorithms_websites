6f64619ef4a036fd69fdc574f3bf9754d56f2703
bong6981 <kbk5190@naver.com>
2021-08-07 13:22:11 +0900

    Delete old solution in hide_cell_phone_number.py(핸드폰번호가리기)

--- bongf/python/level1/hide_cellphone_number.py
+++ bongf/python/level1/hide_cellphone_number.py
@@ -1,13 +1,2 @@
 def solution(phone_number):
     return '*' * (len(phone_number) - 4) + phone_number[-4:]
-
-print('*' * (len('hell') -4))
-
-def solution_old(phone_number):
-    phone_list = list(phone_number)
-    n = len(phone_list)
-    for i in range(n-4):
-        phone_list[i] = '*'
-    str_phone_list = ''.join(phone_list)
-    answer = str_phone_list
-    return answer
