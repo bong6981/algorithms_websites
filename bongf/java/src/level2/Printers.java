233b95bafb0da3802ee970bc325a9e759a94ed39
bong6981 <kbk5190@naver.com>
2021-08-20 19:07:50 +0900

    Complete printers.py(프린터)

---
+++ bongf/java/src/level2/Printers.java
@@ -0,0 +1,44 @@
+package level2;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Printers {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{2, 1, 3, 2},2));
+        System.out.println(solution(new int[]{1, 1, 9, 1, 1, 1},0));
+
+    }
+
+    //파이썬 풀이보고 구현함 
+    public static int solution(int[] priorities, int location) {
+        Queue<Integer> queue = new LinkedList<>(Arrays.stream(priorities).boxed().collect(Collectors.toList()));
+
+        List<Integer> max = Arrays.stream(priorities)
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+
+        int answer = 0;
+        while (true) {
+            int c = queue.poll();
+            if (c == max.get(0)) {
+                answer++;
+                if (location == 0) {
+                    break;
+                } else {
+                    location--;
+                }
+                max.remove(0);
+            } else {
+                queue.offer(c);
+                if (location == 0) {
+                    location = queue.size() - 1;
+                } else {
+                    location--;
+                }
+            }
+        }
+        return answer;
+    }
+}
---
+++ bongf/python/level2/printers.py
@@ -0,0 +1,42 @@
+def solution(priorities, location):
+    count = 0 
+    while True :
+        max_index = priorities.index(max(priorities))
+        if(location < max_index) :
+            location = len(priorities) -1 - max_index + location  
+        elif(location == max_index) :
+            return count+1
+        else : 
+            location -= (max_index+1)
+        priorities = priorities[max_index+1:] + priorities[0:max_index]
+        count += 1
+
+    return count
+
+## popleft()대신 pop(0)을 쓸 수 있으나, list의 마지막까지 가는 것이라 시간 복잡도가 너무 많이 소요된다. 
+## popleft()를 써주자 
+from collections import deque 
+def solution2(priorities, location):
+    queue = deque([(i, p) for i, p in enumerate(priorities)])
+    answer = 0
+
+    while True :
+        cur = queue.popleft()
+        if any(cur[1] < q[1] for q in queue) :
+            queue.append(cur)
+        else :
+            answer += 1
+            if(cur[0] == location) :
+                return answer
+        print(cur)
+
+
+print(solution2([2, 1, 3, 2], 2))
+print(solution2([1, 1, 9, 1, 1, 1], 0))
+
+
+# priorities = [3,1,2,1]
+# max_index = priorities.index(max(priorities))
+# print(max_index)
+# priorities = priorities[(max_index+1):] + priorities[0:(max_index)]
+# print(priorities)
