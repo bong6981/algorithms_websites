851ab9f0295ea19a2c39a45cf8cc45553c01c1f5
bong6981 <kbk5190@naver.com>
2021-09-06 09:12:22 +0900

    Stduy Java permutation

---
+++ bongf/java/src/study/PermutationAll.java
@@ -0,0 +1,21 @@
+package study;
+
+import java.util.HashSet;
+
+public class PermutationAll {
+    public static void main(String[] args) {
+        String numbers = "011";
+        HashSet<Integer> set = new HashSet<>();
+        permutation("", numbers, set);
+    }
+
+    public static void permutation(String prefix, String str, HashSet<Integer> set) {
+        int n = str.length();
+        //if (n == 0) System.out.println(prefix);
+        if(!prefix.equals("")) set.add(Integer.valueOf(prefix));
+        System.out.println("set은" + set);
+        for (int i = 0; i < n; i++) {
+            permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1, n), set);
+        }
+    }
+}
--- bongf/python/level2/stock_price.py
+++ bongf/python/level2/stock_price.py
@@ -2,7 +2,6 @@ def solution(prices):
     answer = []
     for i in range(len(prices)-1) :
         time = 0 
-        ## 마지막 인덱스일때는 if로 빼기 
         for j in range(i+1, len(prices)) :
             time += 1 
             if(prices[j] < prices[i]) :
