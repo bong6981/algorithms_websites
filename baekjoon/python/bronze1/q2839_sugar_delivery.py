1ddbaec9ff1e84be491ed208dabc00bb16488702
bong6981 <kbk5190@naver.com>
2021-12-28 09:58:38 +0900

    Complete baekjoon/bronze1/q2839_sugar_delivery(설탕배달)

---
+++ baekjoon/python/bronze1/q2839_sugar_delivery.py
@@ -0,0 +1,15 @@
+def solution():
+    n = int(input())
+    q, r = divmod(n, 5)
+    possible = False
+    while q >= 0:
+        if r % 3 == 0 :
+            possible = True
+            break
+        q -= 1
+        r += 5
+    if not possible :
+        return -1 
+    return q + r // 3
+
+print(solution())
