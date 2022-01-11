99b03be2705dee3ccaad8d16834d9a261ba1f444
bong6981 <kbk5190@naver.com>
2022-01-11 23:04:43 +0900

    baekjoon/silver2/q11403_find_path(경로찾기)

---
+++ baekjoon/python/silver2/q11403_find_path.py
@@ -0,0 +1,37 @@
+# https://www.acmicpc.net/problem/11403
+from collections import deque
+def solution():
+    n = int(input())
+    INF = 1e9
+    board = []
+    for _ in range(n):
+        board.append(list(map(int, input().split())))
+    
+    for i in range(n):
+        for des in find(i, board, n):
+            board[i][des] = 1
+    
+    for r in board:
+        for c in r :
+            print(c, end=" ")
+        print()
+
+def find(i, board, n):
+    des = set()
+    visited = [False for _ in range(n)]
+    q = deque([i])
+
+    while q:
+        now = q.popleft()
+        visited[now] = True
+        for to, possible in enumerate(board[now]):
+            if possible :
+                des.add(to)
+                if not visited[to]:
+                    des.add(to)
+                    q.append(to)
+    return des
+
+solution()
+# 10:26 소요시간 22분 
+
