f34ff90c0f43edb57f71e7bf79fcf9dd46cf5d24
bong6981 <kbk5190@naver.com>
2022-04-06 11:33:39 +0900

    Update baekjoon/python/silver5/q10989_sort_numbers3.py

--- baekjoon/python/silver5/q10989_sort_numbers3.py
+++ baekjoon/python/silver5/q10989_sort_numbers3.py
@@ -1,9 +1,13 @@
 import sys
 input = sys.stdin.readline
 
+arr = [0] * 10001 
+n = int(input())
+for _ in range(n):
+    num = int(input())
+    arr[num] += 1
 
-
-def sol_메모리초과():
-    n = int(input())
-    arr = [int(input()) for _ in range(n)]
-    for e in sorted(arr): print(e)
+for i in range(10001):
+    if arr[i] > 0:
+        for _ in range(arr[i]):
+            sys.stdout.write(str(i) + '\n')
