628da65642736f8e8c76d17660d5925df1803f5c
bong6981 <kbk5190@naver.com>
2022-04-12 14:25:09 +0900

    baekjoon/java/silver4/Q1920_FindNumber.java(수찾기)

--- baekjoon/java/src/bronze2/Q2231_DissembledSum.java
+++ baekjoon/java/src/bronze2/Q2231_DissembledSum.java
@@ -1,61 +1,21 @@
-package silver4;
+package bronze2;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Q1920_FindNumber {
+public class Q2231_DissembledSum {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
     static void input() {
         N = scan.nextInt();
-        a = new int[N];
-        for (int i = 0; i < N; i++) {
-            a[i] = scan.nextInt();
-        }
-        M = scan.nextInt();
-        nums = new int[M];
-        for (int i = 0; i < M; i++) {
-            nums[i] = scan.nextInt();
-        }
     }
 
-    static int N, M;
-    static int[] a, nums;
+    static int N;
 
-    static void sol() {
-        // 찾는 것도 sort 한 후 이분탐색이 더 빠를까
-        // 찾는 것도 sort 한후 while문이 더 빠를까
-        // 단순 이분탐색이 빠를까
-        Arrays.sort(a);
-        for (int n : nums) {
-            System.out.println("n = " + n);
-            sb.append(find(n)).append("\n");
-        }
-    }
-
-    static int find(int n) {
-        int start = 0;
-        int end = N - 1;
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (a[mid] == n) {
-                return 1;
-            }
-            if (a[mid] < n) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        return 0;
-    }
 
     public static void main(String[] args) {
         input();
-        sol();
-        System.out.println(sb.toString());
     }
 
     static class FastReader {
