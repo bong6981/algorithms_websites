ea980bcac611b7a367bc9d0141371228feeb59c4
bong6981 <kbk5190@naver.com>
2022-04-15 23:43:26 +0900

    baekjoon/python/silver2/q11725_find_tree_parent(트리의부모찾기)

--- baekjoon/python/silver2/q11725_find_tree_parent.py
+++ baekjoon/python/silver2/q11725_find_tree_parent.py
@@ -1,24 +1,27 @@
 from collections import deque
-def solution():
-    n = int(input())
-    board = [[] for _ in range(n+1)]
-    for _ in range(n-1):
-        x, y = map(int, input().split())
-        board[x].append(y)
-        board[y].append(x)
+import sys
 
-    q = deque()
-    parent = [0] * (n+1)
-    for i in board[1]:
-        parent[i] = 1
-        q.append(i)
-    while q:
-        now = q.pop()
-        for i in board[now]:
-            if parent[now] != i :
-                parent[i] = now
-                q.append(i)
-    for k in range(2, n+1):
-        print(parent[k])
+input = sys.stdin.readline
 
-solution()
+N = int(input())
+graph = [[] for _ in range(N+1)]
+parent = list(range(N+1))
+
+for _ in range(N-1):
+    x, y = map(int, input().rstrip().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+queue = deque()
+queue.append(1)
+
+while queue:
+    p = queue.popleft()
+    for ch in graph[p]:
+        if parent[ch] == ch:
+            parent[ch] = p
+            queue.append(ch)
+
+for p in parent[2:]:
+    print(p)
+
