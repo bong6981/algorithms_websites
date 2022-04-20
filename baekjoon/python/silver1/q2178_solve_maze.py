da91459b0592bee10bd43030100193537ba34321
bong6981 <kbk5190@naver.com>
2022-04-20 11:08:16 +0900

    baekjoon/python/silver1/q2178_solve_maze(미로 탐색)

--- baekjoon/python/silver1/q2178_solve_maze.py
+++ baekjoon/python/silver1/q2178_solve_maze.py
@@ -15,7 +15,7 @@ while que:
         ny = y + m[1]
         if 0 <= nx < N and 0 <= ny < M:
             if graph[nx][ny]:
-                if graph[nx][ny] == 1 or graph[x][y] + 1 < graph[nx][ny]:
+                if graph[nx][ny] == 1 :
                     graph[nx][ny] = graph[x][y] + 1
                     que.append((nx, ny))
 print(graph[N-1][M-1])
