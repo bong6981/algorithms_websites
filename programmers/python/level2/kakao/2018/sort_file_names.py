87e9e2bf44f17f243709e52918c12b4189df077b
bong6981 <kbk5190@naver.com>
2022-06-19 13:55:26 +0900

    update programmers/python/level2/kakao/2018/sort_file_names.py

--- programmers/python/level2/kakao/2018/sort_file_names.py
+++ programmers/python/level2/kakao/2018/sort_file_names.py
@@ -55,3 +55,39 @@ print(solution(["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt I
 # print(arr) ## [['bye', 3], ['hello', 2], ['hello', 3]]
 
 
+## 0619
+# def solution(files):
+#     sort_info = []
+#     for k in range(len(files)):
+#         f = files[k]
+#         info = []
+#         idx = 0
+#         for i in range(len(f)):
+#             if f[i].isdigit():
+#                 idx = i
+#                 break
+#         info.append(f[:idx].lower())
+#         num_idx = 0
+#         for i in range(idx, idx+5):
+#             num_idx = i
+#             if not f[i].isdigit() :
+#                 num_idx = i-1
+#                 break
+
+#         # print(idx, num_idx+1, f, f[idx:num_idx+1])
+#         info.append(int(f[idx:num_idx+1]))
+#         info.append(k)
+#         sort_info.append(info)
+    
+#     sort_info.sort()
+#     answer = []
+#     for e1, e2, idx in sort_info:
+#         answer.append(files[idx])
+#     return answer
+
+
+# print(solution(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]))
+
+
+
+
