7dfa4422bda29055d7b2a83d1d58301ab5f01e7f
bong6981 <kbk5190@naver.com>
2022-06-21 23:50:53 +0900

    programmers/python/level3/etc/best_set.py

---
+++ programmers/python/level3/etc/best_set.py
@@ -0,0 +1,16 @@
+## https://programmers.co.kr/learn/courses/30/lessons/12938
+def solution(n, s):
+    
+    m, l = divmod(s, n)
+    
+    if m == 0:
+        return [-1]
+    ans = [m] * n
+    if l == 0:
+        return ans 
+    
+    idx = len(ans) -1 
+    for _ in range(l):
+        ans[idx] += 1
+        idx -= 1
+    return ans
